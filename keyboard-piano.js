f5922b0bb7c127cceeecdb9ecea776247841e29f
Switch-front-end <118104068+Switch-front-end@users.noreply.github.com>
2022-12-07 02:51:37 +0500

    Add files via upload

---
+++ keyboard-piano.js
@@ -0,0 +1,162 @@
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 87 || e.keyCode == 81 || e.keyCode == 69) {
+        document.querySelector('.piano-1').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 82 || e.keyCode == 84 || e.keyCode == 89) {
+        document.querySelector('.piano-2').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 65 || e.keyCode == 83 || e.keyCode == 68) {
+        document.querySelector('.piano-3').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 70 || e.keyCode == 71 || e.keyCode == 72) {
+        document.querySelector('.piano-4').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76) {
+        document.querySelector('.piano-5').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 90 || e.keyCode == 88 || e.keyCode == 67) {
+        document.querySelector('.piano-6').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 86 || e.keyCode == 66 || e.keyCode == 78) {
+        document.querySelector('.piano-7').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 77 || e.keyCode == 32 || e.keyCode == 18) {
+        document.querySelector('.piano-8').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 48 || e.keyCode == 49 || e.keyCode == 50) {
+        document.querySelector('.piano-9').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 51 || e.keyCode == 52 || e.keyCode == 53) {
+        document.querySelector('.piano-10').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 54 || e.keyCode == 55 || e.keyCode == 56) {
+        document.querySelector('.piano-11').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 57 || e.keyCode == 8 || e.keyCode == 36) {
+        document.querySelector('.piano-12').play();
+    }
+});
+
+
+
+// Another twelve lines //
+
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 107 || e.keyCode == 109 || e.keyCode == 110) {
+        document.querySelector('.piano-11').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 111 || e.keyCode == 112 || e.keyCode == 113) {
+        document.querySelector('.piano-12').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 114 || e.keyCode == 115 || e.keyCode == 116) {
+        document.querySelector('.piano-13').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 117 || e.keyCode == 119 || e.keyCode == 120) {
+        document.querySelector('.piano-14').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 121 || e.keyCode == 122 || e.keyCode == 123) {
+        document.querySelector('.piano-15').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 144 || e.keyCode == 145 || e.keyCode == 182) {
+        document.querySelector('.piano-16').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 186 || e.keyCode == 187 || e.keyCode == 188) {
+        document.querySelector('.piano-17').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 189 || e.keyCode == 190 || e.keyCode == 191) {
+        document.querySelector('.piano-18').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 219 || e.keyCode == 220 || e.keyCode == 221) {
+        document.querySelector('.piano-19').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 222 || e.keyCode == 27 || e.keyCode == 33) {
+        document.querySelector('.piano-20').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 34 || e.keyCode == 35 || e.keyCode == 16) {
+        document.querySelector('.piano-21').play();
+        document.querySelector('.piano-23').play();
+    }
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 13 || e.keyCode == 9 || e.keyCode == 91) {
+        document.querySelector('.piano-22').play();
+        document.querySelector('.piano-24').play();
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
